83ecaca7600b4529b6c6f62d54b269493ab7e010
Véro Brossard <brossard.vero.pro@laposte.net>
2021-01-26 14:56:00 +0100

    ecriture des fonctions pour la création et l'accès au tableau des membres

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -1,42 +1,209 @@
 <?php
+function CreatTabMembers(){
 
-$ArrayMembers = [
-  ['index' => '02', 'name' => 'Aisne'],
-  ['index' => '59', 'name' => 'Nord'],
-  ['index' => '60', 'name' => 'Oise'],
-  ['index' => '62', 'name' => 'Pas-de-Calais'],
-  ['index' => '80', 'name' => 'Somme']
+  $ArrayMembers = [
+  ['lastname' => 'karenine', 
+  'firstname' => 'anna', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'anna.karenine@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_00.jpg', 
+  'description' => 'je suis très mignonne...'],
+  
+  ['lastname' => 'granger', 
+  'firstname' => 'hermione', 
+  'age' => '27ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'hermione.granger@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_01.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'everdeen ', 
+  'firstname' => 'katniss', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'katniss.everdeen@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_02.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'connor', 
+  'firstname' => 'sarah', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'sarah.connor@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_03.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'sterling', 
+  'firstname' => 'clarisse', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'clarisse.sterling@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_04.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'jones', 
+  'firstname' => 'bridget', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'bridget.jones@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_05.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'dashwood', 
+  'firstname' => 'marianne', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'marianne.dashwood@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_06.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'chase', 
+  'firstname' => 'annabeth', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'annabeth.chase@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_07.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'desmarets', 
+  'firstname' => 'clémence', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'clemence.desmarets@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_08.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'Undómiel', 
+  'firstname' => 'Arwen', 
+  'age' => '25ans, ', 
+  'gender' =>'femme', 
+  'mail' => 'Arwen.Undomiel@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_09.jpg', 
+  'description' => 'je suis très mignonne...'],
+
+  ['lastname' => 'skywalker', 
+  'firstname' => 'anakin', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'anakin.skywalker@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_10.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'sparrow', 
+  'firstname' => 'jack', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'jack.sparrow@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_11.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'bond', 
+  'firstname' => 'james', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'james.bond@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_12.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'brown', 
+  'firstname' => 'emmet', 
+  'age' => '55ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'emmet.brown@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_13.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'lecter', 
+  'firstname' => 'hannibal', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'hannibal.lecter@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_14.jpg', 
+  'description' => 'je suis trèèèèèès sympa...'],
+
+  ['lastname' => 'callahan', 
+  'firstname' => 'harry', 
+  'age' => '55ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'harry.callahan@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_15.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'karenine', 
+  'firstname' => 'anna', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'anna.karenine@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_16.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'jones', 
+  'firstname' => 'indiana', 
+  'age' => '45ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'indiana.jones@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_17.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'verbal', 
+  'firstname' => 'kint', 
+  'age' => '35ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'kint.verbal@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_18.jpg', 
+  'description' => 'je suis très sympa...'],
+
+  ['lastname' => 'lannister', 
+  'firstname' => 'tyrion', 
+  'age' => '25ans, ', 
+  'gender' =>'homme', 
+  'mail' => 'tyrion.lannister@mail.com', 
+  'zipcode' => '76600', 
+  'picture' => 'member_19.jpg', 
+  'description' => 'je suis très sympa...'],
+
 ];
-/*
-lastname => 'DOE'
-firstname => 'John'
-age => '33ans'
-gender => 'homme'
-mail => 'mail@mail.com'
-zipcode => '76600'
-picture => 'monimage.jpg' !! ATTENTION !!, il va falloir mettre le nom de la photo que vous allez enregistrer dans votre assets/img
-description => 'Je suis un homme qui ...'
-*/
-
-
-foreach ($ArrayMembers as $order => $value) 
-    // on acquiert chaque lg du tableau HtdeFr et son ss-tab ($value) associé.
-{
-  // var_dump($order);   // order est un integer (rang du ss-tableau)
-  // var_dump($value);  // contient les clés et valeurs de la ligne étudiée.
-
-  echo ' [' . ($order) . '] --- ';
-  foreach ($value as $key => $infos)  //on acquiert chaque element (clé/valeur) du ss-tab 
-  {
-    // var_dump($key);
-    // var_dump($infos);
-    echo '(' . $key . ')     ' . $infos . ' ';
-  }
-  echo '<br>';
+return $ArrayMembers;
+//var_dump($ArrayMembers);
 }
-// echo 'Fin du tableau<br>';
 
-var_dump($ArrayMembers);
+function extractMember($arrayName, $indMember){
+ //extrait les infos du ($indMember+1)ème membre du [array] passé en paramétre.
+   //$sous_util = ['lastname' => 'DOE',  etc.. 'description' => 'je suis très']
+   $sous_util = $arrayName[$indMember];
+   //var_dump( $sous_util);
+   return $sous_util;
+
+}
+
+function showMemberDetails($arrayName, $info){
+ //extrait et/ou affiche le champ '$info' du [array] à 1 dimension passé en paramétre.
+   $myinfo = $arrayName[$info];
+
+  //var_dump( $sous_util);
+  return $myinfo;
+
+}
 
 //$nbrlg = count($HtdeFr); pour compter le nbre de lg d'un tableau
 
--- index.php
+++ index.php
@@ -8,8 +8,21 @@
 
 <body>
   création et affichage du tableau des zamoureux...<br>
-<?php include('controllers/lovers_controller.php'); ?>
+<?php require('controllers/lovers_controller.php'); ?>
 
+<?php
+ $ArrayMembersNEW = CreatTabMembers();
+//var_dump($ArrayMembersNEW);
 
+$MemberInfo = extractMember($ArrayMembersNEW, 10);
+var_dump($MemberInfo);
+
+$lastname = showMemberDetails($MemberInfo, 'lastname');
+$firstaname = showMemberDetails($MemberInfo, 'firstname');
+$picture = showMemberDetails($MemberInfo, 'picture');
+var_dump($picture);
+?>
+Bonjour <?= $firstaname . ' ' . ucwords($lastname) . '<br>' ?>
+<img src="assets/img/member_06.jpg" alt="photo du membre"><br>
 </body>
 </html>
