5e6801e7d4558309839b71eb46a79004de9b2534
Véro Brossard <brossard.vero.pro@laposte.net>
2021-01-27 21:28:37 +0100

    finalisation de ma partie user et tests sur index.php

--- index.php
+++ index.php
@@ -1,80 +1,97 @@
 <!doctype html>
 <html lang="fr">
 
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="assets/css/style.css">
-    </head>
-    <body>
-      
-      <div class="ndPrincipal">
-        <div class="row  m-4 p-0">
-          <div class="col-12 col-lg-7 mb-4">
-            <img src="assets/img/arabesque.png" style="width : 80%;"/>
-            <div class="ndTextAccueil">
-              <p>Vivez l'amour</p>
-              <p>avec un grand</p>
-              <p id="ndMajA">A</p>
-            </div>
-            <img src="assets/img/arabesqueRetourner.png" style="width : 80%;"/>
-          </div>
-          <div class="col-12 col-lg-5 mt-4">
-            <div class="ndContForm container d-flex text-center">
-              <form class="mx-auto ndForm text-center" method="post" action="views/user.php">
-                <p> Inscrivez-Vous </p>
-                <div class="row mt-4">
-                  <div class="col-6">
-                    <input type="text" class="form-control" name="lastname" placeholder="Nom de Famille" value="lastname">
-                  </div>
-                  <div class="col-6">
-                    <input type="text" class="form-control" name="firstname" placeholder="Prénom" value="firstname">
-                  </div>
-                </div>
-                <div class="row mt-4">
-                  <div class="col-6">
-                    <input type="text" class="form-control" name="age" placeholder="Age" value="17">
-                  </div>
-                  <div class="col-6">
-                    <select id="gender" name="gender" class="form-control">
-                      <option selected>Homme</option>
-                      <option>Femme</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="row mt-4  mb-4">
-                  <div class="col-6">
-                    <input type="text" class="form-control" name="zipCode" placeholder="Code Postal" value="93000">
-                  </div>
-                  <div class="col-6">
-                    <input type="text" class="form-control" name="mail" placeholder="Adresse Email" value="email@truc.net">
-                  </div>
-                </div>
-                <p>Vous etes intéressé par&nbsp;:</p>
-                <div class="row mt-4  mb-4">
-                  <div class="col-6">
-                    <input type="radio" name="genderSearch" id="genderSearch" value="Homme" checked>
-                    <label for="genderSearch"> Homme </label>
-                  </div>
-                  <div class="col-6">
-                    <input type="radio" name="genderSearch" id="genderSearch" value="Femme">
-                    <label for="genderSearch"> Femme </label>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-12">
-                    <button class="btn" type="submit">Trouve l'Amour</button>
-                  </div>
-                </div>
-              </form> 
-            </div>  
-          </div>
-  </div>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<body>
+  <?php
+  if (!empty($_COOKIE["arrayInfoUser"])) {
+    //cookie validde =>  les membres (lovers.php)
+     header("Location: views/lovers.php");
+  } elseif (!empty($_POST)) {
+    // 1ere visite, le formulaire vient d'être envoyé
+    require('controllers/index_controller.php');
+    setUserCookie(); // fonction définie dans index_controller.php
+    //recupération des valeurs de  $_POST<br>
+    // Déclaration d'un tableau associatif pour les info du user
+    // et création du cookie puis => les membres (lovers.php)
+   header("Location: views/lovers.php");
+  } else {
+    //affichage du formulaire à remplir 
+  ?>
+    <div class="ndPrincipal">
+      <div class="row  m-4 p-0">
+        <div class="col-12 col-lg-7 mb-4">
+          <img src="assets/img/arabesque.png" style="width : 80%;" />
+          <div class="ndTextAccueil">
+            <p>Vivez l'amour</p>
+            <p>avec un grand</p>
+            <p id="ndMajA">A</p>
+          </div>
+          <img src="assets/img/arabesqueRetourner.png" style="width : 80%;" />
+        </div>
+        <div class="col-12 col-lg-5 mt-4">
+          <div class="ndContForm container d-flex text-center">
+            <form class="mx-auto ndForm text-center" method="post" action="">
+              <p> Inscrivez-Vous </p>
+              <div class="row mt-4">
+                <div class="col-6">
+                  <input type="text" class="form-control" name="lastname" placeholder="Nom de Famille" value="lastname">
+                </div>
+                <div class="col-6">
+                  <input type="text" class="form-control" name="firstname" placeholder="Prénom" value="firstname">
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-6">
+                  <input type="text" class="form-control" name="age" placeholder="Age" value="17">
+                </div>
+                <div class="col-6">
+                  <select id="gender" name="gender" class="form-control">
+                    <option selected>Homme</option>
+                    <option>Femme</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row mt-4  mb-4">
+                <div class="col-6">
+                  <input type="text" class="form-control" name="zipCode" placeholder="Code Postal" value="93000">
+                </div>
+                <div class="col-6">
+                  <input type="text" class="form-control" name="mail" placeholder="Adresse Email" value="email@truc.net">
+                </div>
+              </div>
+              <p>Vous etes intéressé par&nbsp;:</p>
+              <div class="row mt-4  mb-4">
+                <div class="col-6">
+                  <input type="radio" name="genderSearch" id="genderSearch" value="Homme" checked>
+                  <label for="genderSearch"> Homme </label>
+                </div>
+                <div class="col-6">
+                  <input type="radio" name="genderSearch" id="genderSearch" value="Femme">
+                  <label for="genderSearch"> Femme </label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <button class="btn" type="submit">Trouve l'Amour</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    <?php
+  }
+    ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- views/user.php
+++ views/user.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang=fr>
 
@@ -12,64 +11,69 @@
 
 <body class="VbBodyUser">
 
-<?php 
-
-if (!empty($_POST)) {
-  
-  require ('../controllers/user_controller.php'); 
-
-//recupération des valeurs de  $_POST<br>
-// $lastname = $_POST['lastname'];
-// $firstname = $_POST['firstname'];
-
-// Déclaration d'un tableau associatif
-$arrayInfoUser= array(
-  'lastname' => ucfirst(htmlspecialchars($_POST['lastname'])), 
-  'firstname' => htmlspecialchars($_POST['firstname']), 
-  'age' => htmlspecialchars($_POST['age']),
-  'gender' => htmlspecialchars($_POST['gender']), 
-  'mail' => htmlspecialchars($_POST['mail']), 
-  'zipCode' => htmlspecialchars($_POST['zipCode']),  
-  'genderSearch' => htmlspecialchars($_POST['genderSearch'])
-);
-
-setcookie('arrayInfoUser', serialize($arrayInfoUser), time() + 24 * 3600); //  ** pour 2jours
-include("../views/lovers.php");
-?>
-
   <div class="container">
-    <div class="row ">
-      <div class="col">
-        <div class="text-center">
-          <h1>Bienvenue sur notre site </h1>
+    <?php
+    if (!empty($_COOKIE['arrayInfoUser'])) {
+      //echo "cookie arrayInfoUser  pas vide";
+      // formulaire  rempli ou visiteur déja venu =>lecture du cookie
+      // var_dump($_COOKIE['arrayInfoUser']);
+      //require('../controllers/user_controller.php');
+      //lecture du cookie et extraction des infos du user par fonction dans user_controller.php
+      $arrayInfoUser = unserialize($_COOKIE['arrayInfoUser']);
+      //var_dump($arrayInfoUser);
+      $lastname = ucfirst($arrayInfoUser['lastname']);
+      $firstname = ucfirst($arrayInfoUser['firstname']);
+      $age = ucfirst($arrayInfoUser['age']);
+      $gender = ucfirst($arrayInfoUser['gender']);
+      $mail = ucfirst($arrayInfoUser['mail']);
+      $zipCode = ucfirst($arrayInfoUser['zipCode']);
+      $genderSearch = ucfirst($arrayInfoUser['genderSearch']);
+      if ($genderSearch == 'homme') {
+        $search = "la Femme";
+      } else {
+        $search = "l'Homme";
+      }
+    ?>
+
+      <div class="row">
+        <div class="col">
+          <div class="text-center" style="color:black;">
+            <h1>Bienvenue sur notre site </h1>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="row ">
-      <div class="col">
-        <div class="bg-light text-center">
- 
-        Bonjour
-nom: <?= ucfirst($arrayInfoUser['lastname']) ?> et prénom :<?= $arrayInfoUser['firstname'] ?><br>
-  <br>
-    
+      <div class="row">
+        <div class="col">
+          <div class="bg-light text-center" style="color:black;">
+            <section>
+              <p><br>
+                Bonjour <?= $firstname ?> <?= $lastname ?> <br>
+                vous avez <?= $age ?> ans et vous habitez à <?= $zipCode ?><br>
+                votre mail (info privée) est : <?= $mail ?><br>
+                J'espère que vous trouverez sur notre site <?= $search ?> que vous attendez.<br> <br>
+              </p>
+            </section>
+            <?php
+            // ****** destruction cookies par fonction dans user_controller.php (bouton à mettre en oeuvre)
+            //deleteCookie();
+
+            ?>
+          </div>
         </div>
       </div>
-    </div>
+    <?php
+
+    } else {   //   le formulaire n'a pas été rempli
+      //   ***  retour au formulaire à remplir => index.php... ***
+      header("Location: ../index.php");  //formulaire non rempli !!
+    }
+    ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </div>
-
-<?php
-
-}
-else{
-  header("Location: ../index.php");  //formulaire non rempli !!
-}
-?>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
